5179cb0b500e53804af693f4f617c4b22719ec20
zcong-pateo <2691371547@qq.com>
2018-06-21 15:56:23 +0800

    '能够正常跑webpack配置文件'

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: "./src/js/index.js",
   output: {
-    path: __dirname,
-    filename: "dist/js/bundle.js"
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js'
   },
-  module: {
-    loaders: [
-      // { test: /\.css$/, loader: "style-loader!css-loader" }
-    ]
-  }
+  plugins:[
+    new HtmlWebpackPlugin({
+      filename:'../index.html'  //以output为根目录
+    }),
+  ]
 };
